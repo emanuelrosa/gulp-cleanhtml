ef220c5f91ed6c491620e1386bf766cd7b7925ef
Fernando Gusmao <fenix.gusmao+beanstalkapp@gmail.com>
2016-10-01 12:21:45 -0300

    test unprotect option

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var htmlClean = require('htmlclean');
 module.exports = function() {
   'use strict';
   return es.map(function (file, cb) {
-    file.contents = new Buffer(htmlClean(file.contents.toString()));
+    file.contents = new Buffer(htmlClean(file.contents.toString(),{
+      unprotect: /<(script|style)[\s\S]+?<\/(script|style)>/ig
+    }));
     cb(null,file);
   });
 };
